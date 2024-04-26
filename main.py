651a0c069bac077d0782aa234f1415673e61e15c
Marco Barbosa <aureliobarbosa@gmail.com>
2024-04-25 22:48:44 -0300

    simple scape with 'q' or 'quit'

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+from interpreter import Interpreter
 from lexer import Lexer
 from parser_ import Parser
-from interpreter import Interpreter
 
 while True:
-	try:
-		text = input("calc > ")
-		lexer = Lexer(text)
-		tokens = lexer.generate_tokens()
-		parser = Parser(tokens)
-		tree = parser.parse()
-		if not tree: continue
-		interpreter = Interpreter()
-		value = interpreter.visit(tree)
-		print(value)
-	except Exception as e:
-		print(e)
+    try:
+        text = input("calc > ").strip()
+
+        if text.isascii():
+            text = text.lower()
+            if text in ["q", "quit"]:
+                break
+
+        lexer = Lexer(text)
+        tokens = lexer.generate_tokens()
+        parser = Parser(tokens)
+        tree = parser.parse()
+        if not tree:
+            continue
+        interpreter = Interpreter()
+        value = interpreter.visit(tree)
+        print(value)
+    except Exception as e:
+        print(e)
