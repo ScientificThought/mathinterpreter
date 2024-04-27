b7edeca6d91976fd5e5ba764af77f41e3a9bf8c6
Marco Barbosa <aureliobarbosa@gmail.com>
2024-04-27 18:57:57 -0300

    Change directory structure

---
+++ mathinterpreter/main.py
@@ -0,0 +1,30 @@
+from interpreter import Interpreter
+from lexer import Lexer
+from parser_ import Parser
+
+
+def main():
+    while True:
+        try:
+            text = input("calc > ").strip()
+
+            if text.isascii():
+                text = text.lower()
+                if text in ["q", "quit"]:
+                    break
+
+            lexer = Lexer(text)
+            tokens = lexer.generate_tokens()
+            parser = Parser(tokens)
+            tree = parser.parse()
+            if not tree:
+                continue
+            interpreter = Interpreter()
+            value = interpreter.visit(tree)
+            print(value)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
