481f70a8639a66280aab7116b28e7f43ba84026e
Marco Barbosa <aureliobarbosa@gmail.com>
2024-04-29 20:43:17 -0300

    refactor lexer tests to pytest (H. Guidi).

--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -1,46 +1,46 @@
-import unittest
-from tokens import Token, TokenType
-from lexer import Lexer
+import pytest
+from mathinterpreter.tokens import Token, TokenType
+from mathinterpreter.lexer import Lexer
 
-class TestLexer(unittest.TestCase):
+class TestLexer():
 	
 	def test_empty(self):
 		tokens = list(Lexer("").generate_tokens())
-		self.assertEqual(tokens, [])
+		assert tokens == []
 	
 	def test_whitespace(self):
 		tokens = list(Lexer(" \t\n  \t\t\n\n").generate_tokens())
-		self.assertEqual(tokens, [])
+		assert tokens == []
 
 	def test_numbers(self):
 		tokens = list(Lexer("123 123.456 123. .456 .").generate_tokens())
-		self.assertEqual(tokens, [
+		assert tokens == [
 			Token(TokenType.NUMBER, 123.000),
 			Token(TokenType.NUMBER, 123.456),
 			Token(TokenType.NUMBER, 123.000),
 			Token(TokenType.NUMBER, 000.456),
 			Token(TokenType.NUMBER, 000.000),
-		])
+		]
 	
 	def test_operators(self):
 		tokens = list(Lexer("+-*/").generate_tokens())
-		self.assertEqual(tokens, [
+		assert tokens == [
 			Token(TokenType.PLUS),
 			Token(TokenType.MINUS),
 			Token(TokenType.MULTIPLY),
 			Token(TokenType.DIVIDE),
-		])
+		]
 
 	def test_parens(self):
 		tokens = list(Lexer("()").generate_tokens())
-		self.assertEqual(tokens, [
+		assert tokens == [
 			Token(TokenType.LPAREN),
 			Token(TokenType.RPAREN),
-		])
+		]
 	
 	def test_all(self):
 		tokens = list(Lexer("27 + (43 / 36 - 48) * 51").generate_tokens())
-		self.assertEqual(tokens, [
+		assert tokens == [
 			Token(TokenType.NUMBER, 27),
 			Token(TokenType.PLUS),
 			Token(TokenType.LPAREN),
@@ -52,4 +52,4 @@ class TestLexer(unittest.TestCase):
 			Token(TokenType.RPAREN),
 			Token(TokenType.MULTIPLY),
 			Token(TokenType.NUMBER, 51),
-		])
+		]
