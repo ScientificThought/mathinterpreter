27bb9616d83c54b81309726935fb8b416dfdbcf7
Marco Barbosa <aureliobarbosa@gmail.com>
2024-05-06 18:07:56 -0300

    Add token POWER to lexer

--- mathinterpreter/lexer.py
+++ mathinterpreter/lexer.py
@@ -56,6 +56,9 @@ class Lexer:
             elif self.current_char == "/":
                 self.advance()
                 yield Token(TokenType.DIVIDE)
+            elif self.current_char == "^":
+                self.advance()
+                yield Token(TokenType.POWER)
             elif self.current_char == "(":
                 self.advance()
                 yield Token(TokenType.LPAREN)
